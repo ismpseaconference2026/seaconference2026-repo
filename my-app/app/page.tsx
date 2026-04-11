1755b9d460bafbacbb0227f8b44533f4606fdb8f
Shazlow <boss2457bc@gmail.com>
2026-04-11 13:19:11 +0700

    add nextjs app

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-yellow-200">
+      <h1 className="text-violet-900 text-7xl font-bold">More info to come soon!</h1>
+    </div>
+  );
+}
